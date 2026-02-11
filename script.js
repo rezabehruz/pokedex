6c2ba7d9e7e2dd2ca1ac72527920b2b3725934e6
Reza <behruz.mreza@gmail.com>
2026-02-11 12:58:13 +0100

    feat: Design Dialog und rendering Data from API.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function renderPokemones() {
       for (let j = 0; j < pokemons[i].types.length; j++) {
         types += `<p> ${pokemons[i].types[j].type.name} </p>`;
       }
-    contentRef.innerHTML += pokemonTemplate(pokemons[i], i, types);
+    contentRef.innerHTML += pokemonTemplate(i, types);
     changeBackground(pokemons[i].types[0].type.name, i);
   }
 
@@ -76,11 +76,6 @@ async function getPokemons(url) {
   }
 }
 
-function showDialog() {
-  document.getElementById("pokemon-dialog").showModal();
-  document.getElementById("pokemon-dialog").classList.add("pokemon-dialog");
-}
-
 function renderPagination() {
   countPaginationRef.innerText = currentPagination + " of " + totalPagination;
 
@@ -92,10 +87,10 @@ function renderPagination() {
       btnBackwardRef.classList.remove("v-none");
       break;
     case totalPagination:
-      btnForwarddRef.classList.add("v-none");
+      btnForwardRef.classList.add("v-none");
       break;
     case totalPagination - 1:
-      btnForwarddRef.classList.remove("v-none");
+      btnForwardRef.classList.remove("v-none");
       break;
     default:
       break;
@@ -124,4 +119,117 @@ function backward() {
   currentPagination--;
   renderStartID -= 20;
   renderPokemones();
+}
+
+const pokemonDialogRef = document.getElementById("pokemon-dialog");
+
+let aboutContentRef, stateContentRef, spriteContentRef;
+let btnAboutRef, btnStatRef, btnSpriteRef;
+let btnNextDialogRef, btnBackDialogRef;
+
+async function showDialog(i, types) {
+  pokemonDialogRef.showModal();
+
+  const pokemonDetails = await getPokemonDetails(i);
+  const about = getPokemonAbout(pokemonDetails);
+  const states = getPokemonStates(pokemonDetails);
+  const sprites = pokemonDetails.sprites;
+
+  pokemonDialogRef.innerHTML = dialogContentTemplate(i, types, about, states, sprites);
+
+  btnAboutRef = document.getElementById("btn-about");
+  btnStatRef = document.getElementById("btn-stat");
+  btnSpriteRef = document.getElementById("btn-sprite");
+  aboutContentRef = document.getElementById("about-content");
+  stateContentRef = document.getElementById("state-content");
+  spriteContentRef = document.getElementById("sprite-content");
+
+  btnNextDialogRef = document.getElementById("btn-next-dialog");
+  btnBackDialogRef = document.getElementById("btn-back-dialog");
+}
+
+function getPokemonAbout(pokemonDetails) {
+  const about = {};
+  about.id = pokemonDetails.id;
+  about.height = pokemonDetails.height;
+  about.weight = pokemonDetails.weight;
+  about.abilities = "";
+  for (let i = 0; i < pokemonDetails.abilities.length; i++) {
+    if (i == 0) about.abilities += pokemonDetails.abilities[i].ability.name;
+    else about.abilities += ", " + pokemonDetails.abilities[i].ability.name;
+  }
+
+  return about;
+}
+
+function getPokemonStates(pokemonDetails) {
+  let stats = "";
+  for (let i = 0; i < pokemonDetails.stats.length; i++) {
+    stats += `<p> ${pokemonDetails.stats[i].stat.name} </p>`;
+  }
+
+  return stats;
+}
+
+async function getPokemonDetails(i) {
+  const response = await fetch(pokemons[i].url);
+  const responseResult = await response.json();
+  return responseResult;
+}
+
+function displayAbout() {
+  btnAboutRef.setAttribute("class", "active-details");
+  btnStatRef.classList.remove("active-details");
+  btnSpriteRef.classList.remove("active-details");
+
+  aboutContentRef.setAttribute("class", "about-content");
+  stateContentRef.setAttribute("class", "d-none");
+  spriteContentRef.setAttribute("class", "d-none");
+}
+
+function displayStats() {
+  btnStatRef.setAttribute("class", "active-details");
+  btnAboutRef.classList.remove("active-details");
+  btnSpriteRef.classList.remove("active-details");
+  stateContentRef.setAttribute("class", "state-content");
+  aboutContentRef.setAttribute("class", "d-none");
+  spriteContentRef.setAttribute("class", "d-none");
+}
+
+function displaySprites() {
+  btnSpriteRef.setAttribute("class", "active-details");
+  btnAboutRef.classList.remove("active-details");
+  btnStatRef.classList.remove("active-details");
+
+  spriteContentRef.setAttribute("class", "sprite-content");
+  aboutContentRef.setAttribute("class", "d-none");
+  stateContentRef.setAttribute("class", "d-none");
+}
+
+function nextPokemon(i) {
+  if (i < pokemons.length) {
+    let types = "";
+    if (pokemons[i].types.length > 0)
+      for (let j = 0; j < pokemons[i].types.length; j++) {
+        types += `<p> ${pokemons[i].types[j].type.name} </p>`;
+      }
+
+    showDialog(i, types);
+  } else {
+    btnNextDialogRef.setAttribute("class", "v-none");
+  }
+}
+
+function previousPokemon(i) {
+  if (i >= 0) {
+    let types = "";
+    if (pokemons[i].types.length > 0)
+      for (let j = 0; j < pokemons[i].types.length; j++) {
+        types += `<p> ${pokemons[i].types[j].type.name} </p>`;
+      }
+
+    showDialog(i, types);
+  } else {
+    btnBackDialogRef.setAttribute("class", "v-none");
+  }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,12 +1,55 @@
-function pokemonTemplate(pokemon, i, types) {
+function pokemonTemplate(i, types) {
   return /*html*/ `          
-         <article id="pokemon-${i}" class="card card1" onclick="showDialog()">
-            <h2 class="roboto-bold">${pokemon.name}</h2>
+         <article id="pokemon-${i}" class="card card1" onclick="showDialog(${i}, '${types}')">
+            <h2 class="roboto-bold">${pokemons[i].name}</h2>
             <div class="card-details">
               <div class="type-details roboto-semi-bold">
                 ${types}
               </div>
-              <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+              <img src="${pokemons[i].sprites.front_default}" alt="${pokemons[i].name}" />
             </div>
           </article>`;
+}
+
+function dialogContentTemplate(i, types, about, stats, sprites) {
+  return /*html*/ `
+         <div class="content">
+            <div class="head-content">
+              <h2 class="roboto-bold">${pokemons[i].name}</h2>
+              <div class="types-img">
+                <div class="types">
+                  ${types}
+                </div>
+                <img src="${pokemons[i].sprites.front_default}" alt="${pokemons[i].name}" />
+              </div>
+            </div>
+            <div class="main-content">
+              <div class="menu roboto-bold">
+                <a href="#"> <button id="btn-about" class="active-details" onclick="displayAbout()">About</button></a>
+                <a href="#"> <button id="btn-stat" onclick="displayStats()">Base Stats</button></a>
+                <a href="#"> <button id="btn-sprite" onclick="displaySprites()">Sprites</button></a>
+                
+              </div>
+              <div class="details-content">
+                <section id="about-content" class="about-content ">
+                    <p>ID: ${about.id}</p>
+                    <p>Height: ${about.height}</p>
+                    <p>weight: ${about.weight}</p>
+                    <p>abilities: ${about.abilities}</p>
+                  
+                </section>
+                <section id="state-content" class="d-none">${stats}</section>
+                <section id="sprite-content" class="d-none">
+                  <img src="${sprites.back_default}" alt="${pokemons[i]}">
+                  <img src="${sprites.back_shiny}" alt="${pokemons[i]}">
+                  <img src="${sprites.front_shiny}" alt="${pokemons[i]}">
+                </section>  
+              </div>
+            </div>
+            <div class="footer-content">
+              <button id="btn-back-dialog" onclick="previousPokemon(${i-1})">Previous</button>
+              <button id="btn-next-dialog" onclick="nextPokemon(${i+1})">Next</button>
+            </div>
+          </div>
+  `;
 }
